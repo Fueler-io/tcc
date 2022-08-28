76ee12a6719b6152321dc18a3455cd23c047c8f8
unknown <aquib_j@yahoo.com>
2022-08-28 10:59:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,183 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container-fluid main">
+			{/* Header */}
+			<header className="main-header my-3">
+				<div className="main-header-background-image"></div>
+				<div className="container">
+					<div className="main-header-profile mb-4">
+						<div className='d-flex justify-content-between align-items-end'>
+							<div>
+								<img className="rounded-circle mb-4" src="https://cdn-icons-png.flaticon.com/512/3688/3688596.png" alt="" />
+							</div>
+							<div>
+								<a href="" id='website-button' className='btn primary-button px-3 fw-500'>Explore Website</a>
+							</div>
+						</div>
+						<h2 className='fw-bold primary-color'>The Clueless Company</h2>
+						<p className='text-gray fw-500'>Lorem ipsum dolor sit, amet consectetur error voluptas ut sunt libero necessitatibus qui.</p>
+
+						<a href="" id='website-button-botom' className='btn primary-button px-3 fw-500'>Explore Website</a>
+					
+					</div>
+				</div>
+			</header>
+
+
+			<section className='main-body my-3'>
+
+				<div className='main-body-heading text-center py-3'>
+					<h1 className='fw-bold primary-color'>Proof of Work Ideas</h1>
+				</div>
+
+				<div className="container">
+					<div className="row justify-content-center py-4">
+						{/* POW IDea 1 */}
+						<div className="col-md-12 mb-4 pb-2">
+							<div className="card px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149280/fueler-emoji/vfx1bghkro4aredz3mut.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-gray fw-500'>Lorem ipsum dolor sit, amet consectetur error voluptas ut sunt libero necessitatibus  this is an pow idea of development category.</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Development
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-12 mb-4 pb-2">
+							<div className="card px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149280/fueler-emoji/vfx1bghkro4aredz3mut.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-gray fw-500'>Lorem ipsum dolor sit, amet consectetur error voluptas ut sunt libero necessitatibus  this is an pow idea of development category.</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Development
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-12 mb-4 pb-2">
+							<div className="card px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149280/fueler-emoji/vfx1bghkro4aredz3mut.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-gray fw-500'>Lorem ipsum dolor sit, amet consectetur error voluptas ut sunt libero necessitatibus  this is an pow idea of development category.</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Development
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+
+						{/* POW IDea 1 */}
+						<div className="col-md-12 mb-4 pb-2">
+							<div className="card px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149280/fueler-emoji/vfx1bghkro4aredz3mut.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-gray fw-500'>Lorem ipsum dolor sit, amet consectetur error voluptas ut sunt libero necessitatibus  this is an pow idea of development category.</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Development
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+						{/* POW IDea 1 */}
+						<div className="col-md-12 mb-4 pb-2">
+							<div className="card px-3">
+								<div className="card-body">
+									<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+										<div className='d-flex justify-content-start flex-wrap align-items-center'>
+											<div className='card-icon my-3 mr-2'>
+												<img src="https://res.cloudinary.com/kiwismedia/image/upload/v1641149280/fueler-emoji/vfx1bghkro4aredz3mut.png" alt="" />
+											</div>
+											<div className='card-content'>
+												<p className='mb-0 text-gray fw-500'>
+													Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ullam ab quasi minus! Ut ab, quibusdam atque saepe illum laborum perspiciatis cupiditate laboriosam rem quaerat at iste laudantium praesentium! Voluptatem.
+
+												</p>
+											</div>
+										</div>
+										<div className='card-tag'>
+											<div className='card-tag-button px-3 py-2'>
+												Development
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+			</section>
+
+
+			<footer className='my-4'>
+				<div className='text-center pb-5'>
+					<h5 className='mb-0 py-4'>
+						<a href="">
+							Powered by <span><img src="https://fueler.io/images/fueler/main/fueler_logo.svg" width={120} alt="" /></span>
+						</a>
+					</h5>
+				</div>
+			</footer>
+
+		</div>
+	);
 }
 
 export default App;
